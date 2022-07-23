743569eb8b3b2e7f565203b96505babf2e29896b
vml19 <vml19.t@gmail.com>
2022-07-23 18:25:59 +0800

    Refactor

---
+++ basics/ca_basics/SumIntDigits.cs
@@ -0,0 +1,20 @@
+﻿namespace ca_basics
+{
+    internal static class SumIntDigits
+    {
+        /// <summary>
+        /// You are given a two-digit integer n. Return the sum of its digits.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        internal static int Solution(int n)
+        {
+            if (n >= 10 && n <= 99)
+            {
+                var str = n.ToString().Select(c => c - 48).ToArray();
+                return str.Sum();
+            }
+            return n;
+        }
+    }
+}
