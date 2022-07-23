a70e8c137d3924808b898d350c40e179f5686cb4
vml19 <vml19.t@gmail.com>
2022-07-23 18:11:44 +0800

    Initial check-in.

---
+++ basics/ca_basics/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ca_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            Console.WriteLine(SumIntDigits(59));
+        }
+
+        /// <summary>
+        /// You are given a two-digit integer n. Return the sum of its digits.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private static int SumIntDigits(int n)
+        {
+            if (n >= 10 && n <= 99)
+            {
+                var str = n.ToString().Select(c => c-48).ToArray();
+                return str.Sum();
+            }
+            return n;
+        }
+    }
+}
